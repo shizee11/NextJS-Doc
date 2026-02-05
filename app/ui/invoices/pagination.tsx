1da9b6418a67d2b64638f32df395f415794e3bf4
Shizee <shizeeoliviasaucedo@gmail.com>
2026-02-05 17:37:59 -0600

    feat: CH 11 completed - redirect & mutated data

--- app/ui/invoices/pagination.tsx
+++ app/ui/invoices/pagination.tsx
@@ -24,7 +24,7 @@ export default function Pagination({ totalPages }: { totalPages: number }) {
     <>
       {/*  NOTE: Uncomment this code in Chapter 10 */}
 
-      {/* <div className="inline-flex">
+      { /*<div className="inline-flex">
         <PaginationArrow
           direction="left"
           href={createPageURL(currentPage - 1)}
--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -6,15 +6,12 @@ import { useDebouncedCallback } from 'use-debounce';
 
 export default function Search({ placeholder }: { placeholder: string }) {
   const searchParams = useSearchParams();
-  const pathname = usePathname();
   const { replace } = useRouter();
-
-  //function handleSearch(term: string) {
-  const handleSearch = useDebouncedCallback((term: string) => {
-      console.log(`Searching... ${term}`);
-
-      const params = new URLSearchParams(searchParams);
-      params.set('page', '1');
+  const pathname = usePathname();
+ 
+  const handleSearch = useDebouncedCallback((term) => {
+    const params = new URLSearchParams(searchParams);
+    params.set('page', '1');
       if (term) {
         params.set('query', term);
       } else {
